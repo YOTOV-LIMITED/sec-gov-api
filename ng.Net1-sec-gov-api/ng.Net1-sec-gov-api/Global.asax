4b14a60056405951a04ad5c8b0be6d3b1dcfc86f
Ivaylo <ivaylo@yotovlimited.com>
2015-11-06 22:17:00 +0100

    yotov limited

---
+++ ng.Net1-sec-gov-api/ng.Net1-sec-gov-api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ng.Net1_sec_gov_api.WebApiApplication" Language="C#" %>
---
+++ ng.Net1-sec-gov-api/ng.Net1-sec-gov-api/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ng.Net1_sec_gov_api.Startup))]
+
+namespace ng.Net1_sec_gov_api
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
